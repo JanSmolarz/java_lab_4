cb35aa82e825cdf6d76bee6fcf2fdec868e0fe5b
Jan <jan.smolarz@yahoo.com>
2022-01-31 22:53:08 +0100

    zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+
+    public static void metoda1(String narzad, String organ)
+    {
+
+    }
+
+        public static void main(String[] args) {
 
         /* zad.1
         a) Stwożyć interfejs Budynek,
@@ -30,5 +36,32 @@ public class Main {
         d) sprawdzić czy idzie wywołać z obu instancji objektu (deklaracja z interfejsu i z klasy)
         metodę której nie ma zadeklarowane w interfejsie Budynek, ale jest dopisana w klassie Dom,
         */
+
+        Dom dom1 = new Dom();
+        dom1.setValue(7);
+        dom1.setKolor(Budynek.KolorEnum.BIALY);
+        dom1.setCena(150000);
+        dom1.setMiejscowosc("Bełchatów");
+        dom1.setKodPocztowy("97-400");
+        dom1.setNrDomu(1);
+        dom1.setNrLokalu("18");
+        dom1.setPowBudynku(6000.25);
+        dom1.setPowierzchnia(35);
+        dom1.setPowParkingu(100);
+        dom1.setPowPodworka(0);
+        dom1.setUlica("Kredowa");
+        dom1.setNazwaFirmy("Druteks");
+        dom1.setRokBudowy("2015");
+        System.out.println(dom1.toString());
+
+        Budynek dom2 = new Dom();
+        dom2.liczbaOkien(6);
+        dom2.liczbaMieszkancow(5);
+        dom2.kolorDomu(Budynek.KolorEnum.BRAZOWY);
+        dom2.adresBudynku("Bełchatów","Hubala",5, "11", "97-400");
+        dom2.powierzchnia(10000,40,410);
+        dom2.deweloper("JanBud","2021");
+        System.out.println(dom2.toString());
+
     }
 }
