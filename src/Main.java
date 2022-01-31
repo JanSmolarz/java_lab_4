f149e5dd891a8e8ddab349c17920fdba5c5139f8
Jan <jan.smolarz@yahoo.com>
2022-01-31 23:12:43 +0100

    zrobione

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,16 @@ public class Main {
         b) zaimplementować metody abstrakcyjne odpowiednimi wzorami,
         c) stworzyć objekty tych klas i wyświetlić przykładowe wyliczenia
         */
+        Prostokat prostokat1 = new Prostokat();
+        System.out.println(prostokat1.obwod(10,7));
+        System.out.println(prostokat1.pole(10,7));
+        prostokat1.nazwaFigury("Prostokat");
 
+        System.out.println(" ");
+
+        Kwadrat kwadrat1 = new Kwadrat();
+        System.out.println(kwadrat1.obwod(5,5));
+        System.out.println(kwadrat1.pole(5,5));
+        kwadrat1.nazwaFigury("Kwadrat");
     }
 }
